7d8ed8a5de8ff4dd51f2dc975615e7412633e4dd
Eduard <sorokolietov@gmail.com>
2023-12-10 17:43:34 +0100

    commit

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,59 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Зайшли в дім");
+    } else {
+      console.log("Двері замкнені");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Двері відчинені");
+    } else {
+      console.log("Хибний ключ. Двері залишаються зачиненими");
+    }
+  }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
