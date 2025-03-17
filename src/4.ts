b373efa542a7a78bb8b52c9032845a0c59c1bec4
Eduard <sorokolietov@gmail.com>
2025-03-17 16:11:07 +0100

    ts-hw3-v2.0

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) { }
+  
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key)
+  }
+
+  openDoor(key: Key) {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
